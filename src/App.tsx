0290bd2ec05f2674c570611113530f31d061cd8b
Benign X <1341398182@qq.com>
2026-05-04 19:15:11 +0800

    💅(layout): cap main content at 1400px centered so preview + editor don't over-stretch on wide 16:9 displays

--- src/App.tsx
+++ src/App.tsx
@@ -1054,7 +1054,7 @@ function AppInner() {
         }}
       />
 
-      <div className="flex min-h-0 flex-1 flex-col p-2 sm:p-3">
+      <div className="mx-auto flex min-h-0 w-full max-w-[1400px] flex-1 flex-col p-2 sm:p-3">
       <section className="flex min-h-0 min-w-0 flex-1 flex-col gap-3 pb-[calc(3.5rem+env(safe-area-inset-bottom))] lg:pb-0">
         <PlaybackBar
           ref={playbackRef}
